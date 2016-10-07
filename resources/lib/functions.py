d2062c50a4141b1deebad653b989ebbb533e589e
mKoenigIZ <mark.koenig@kleiner-schelm.de>
2016-10-07 17:38:54 +0200

    add libs

---
+++ resources/lib/functions.py
@@ -0,0 +1,306 @@
+import mechanize
+from BeautifulSoup import BeautifulSoup
+
+class ItemClass(object):
+    pass
+
+def getHTML(user, pw, link):
+
+    # init browser
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+
+    br.open("http://www.onlinetvrecorder.com/v2/?go=home")
+
+    # login
+    br.select_form('fhomelogin')
+
+    br['email'] = user
+    br['password'] = pw
+    br.submit().read()
+    
+    response = br.open(link)
+    result = response.read()    
+    
+    return result 
+
+def scanList(data):
+    
+    itemlist = []
+    
+    soup = BeautifulSoup(data)
+    content = soup.findAll ('tr', id=lambda x: x and x.startswith('serchrow'))
+
+    for c in content:
+    
+        x = ItemClass()
+    
+        inp = c.find('input', { 'type' : 'hidden'})
+        x.id = inp['value']
+    
+        title = c.find('p' , id=lambda x: x and x.startswith('ptitleandtext'))
+        x.title = title.find(text=True).strip()
+        #new 05.07.
+        if (len(x.title) == 0):
+            title = c.find('a')
+            x.title = title.text
+    
+        desc = c.find('span' , id=lambda x: x and x.startswith('spanlongtext'))
+        x.desc = desc.text
+    
+        x.thumb = 'DefaultVideo.png' 
+    
+        pict = c.find('td' , id=lambda x: x and x.startswith('listimagetd'))
+        pic = pict.find('img')
+        x.thumb = pic['src']
+    
+        genre = c.find('p' , style=lambda x: x and x.startswith('text-transform:capitalize'))
+        x.genre = genre.text
+    
+        data = c.findAll('td' , oncontextmenu=lambda x: x and x.startswith('showNewTabMenu'))
+        start = ''
+        stop = ''
+        
+        x.serie = ''
+        x.episode = ''
+        x.date = ''
+        x.time = ''
+    
+        for d in data:
+            tmp = d.text
+        
+            if len(tmp) == 3:
+                if(tmp[0] == 'S'):
+                    ser = tmp[1:]
+                    if(ser.isdigit()):
+                        x.serie = ser
+                if(tmp[0] == 'E'):
+                    ep = tmp[1:]
+                    if(ep.isdigit()):
+                        x.episode = ep
+            if len(tmp) == 8:
+                x.date = tmp
+            if len(tmp) == 5:
+                if(start == ''):
+                    start = tmp
+                    x.time = tmp
+                else:
+                    stop = tmp
+       
+        itemlist.append(x)
+    
+    return itemlist
+
+def getSearchString(keyword, page):
+    
+    iPage = int(page)
+    x = (iPage-1) * 20
+    
+    s = 'http://www.onlinetvrecorder.com/v2/?go=list&tab=search'
+    s += '&station='
+    s += '&date=all'
+    s += '&min_start_date=0'
+    s += '&year=2016'
+    s += '&fd=1'
+    s += '&fm=1'
+    s += '&td=31'
+    s += '&tm=12'
+    s += '&actor='
+    s += '&season='
+    s += '&episode='
+    s += '&director='
+    s += '&minutes='
+    s += '&title=' + keyword
+    s += '&genre='
+    s += '&order=beginn%20DESC' # neueste zuerst
+    s += '&format=mp4,mcut,avi,hq,hcut,' # stream format
+    s += '&times='
+    s += '&intext=0'
+    s += '&exact=0'
+    s += '&cbde=0'
+    s += '&cbsing=0'
+    s += '&cben=0'
+    s += '&cbxy=0'
+    s += '&cbfav=0'
+    s += '&rating='
+    s += '&weekday='
+    s += '&searchmethod=match'
+    s += '&indatefrom=0'
+    s += '&indateto=0'
+    s += '&intimefrom=0'
+    s += '&intimeto=23'
+    s += '&source=otr' # suche nur in otr
+    s += '&wdh='
+    s += '&fsk='
+    s += '&productionyear='
+    s += '&filestate='
+    s += '&_view=table'
+    s += '&start=' + str(x)
+    
+    return s
+def getGroupString(group, page):
+    
+    iPage = int(page)
+    x = (iPage-1) * 20
+    
+    s = 'http://www.onlinetvrecorder.com/v2/?go=list&tab=search'
+    s += '&station='
+    s += '&date=all'
+    s += '&min_start_date=0'
+    s += '&year=2016'
+    s += '&fd=1'
+    s += '&fm=1'
+    s += '&td=31'
+    s += '&tm=12'
+    s += '&actor='
+    s += '&season='
+    s += '&episode='
+    s += '&director='
+    s += '&minutes='
+    s += '&title='
+    s += '&genre=' + group
+    s += '&order=beginn%20DESC' # neueste zuerst
+    s += '&format=mp4,mcut,avi,hq,hcut,' # stream format
+    s += '&times='
+    s += '&intext=0'
+    s += '&exact=0'
+    s += '&cbde=0'
+    s += '&cbsing=0'
+    s += '&cben=0'
+    s += '&cbxy=0'
+    s += '&cbfav=0'
+    s += '&rating='
+    s += '&weekday='
+    s += '&searchmethod=match'
+    s += '&indatefrom=0'
+    s += '&indateto=0'
+    s += '&intimefrom=0'
+    s += '&intimeto=23'
+    s += '&source=otr' # suche nur in otr
+    s += '&wdh='
+    s += '&fsk='
+    s += '&productionyear='
+    s += '&filestate='
+    s += '&_view=table'
+    s += '&start=' + str(x)
+    
+    return s
+
+def getRecordsString(page):
+    
+    iPage = int(page)
+    x = (iPage-1) * 20
+   
+    s = 'http://www.onlinetvrecorder.com/v2/?go=list&tab=search'
+    s += '&station='
+    s += '&date=sinceregister'
+    s += '&min_start_date=0'
+    s += '&year=2016'
+    s += '&fd=1'
+    s += '&fm=1'
+    s += '&td=31'
+    s += '&tm=12'
+    s += '&actor='
+    s += '&season='
+    s += '&episode='
+    s += '&director='
+    s += '&minutes='
+    s += '&title='
+    s += '&genre=0'
+    s += '&order=beginn%20DESC' # neueste zuerst
+    s += '&format=mp4,mcut,avi,hq,hcut,' # stream format
+    s += '&times=0'
+    s += '&intext=0'
+    s += '&exact=0'
+    s += '&cbde=0'
+    s += '&cbsing=0'
+    s += '&cben=0'
+    s += '&cbxy=0'
+    s += '&cbfav=0'
+    s += '&rating=0'
+    s += '&weekday='
+    s += '&searchmethod=match'
+    s += '&indatefrom=0'
+    s += '&indateto=0'
+    s += '&intimefrom=0'
+    s += '&intimeto=23'
+    s += '&source=my' # suche nur records
+    s += '&wdh='
+    s += '&fsk='
+    s += '&productionyear='
+    s += '&filestate='
+    s += '&_view=table'
+    s += '&start=' + str(x)
+    
+    return s
+
+
+def getSearchStationString(keyword, station, date, page):
+    
+    iPage = int(page)
+    x = (iPage-1) * 20
+ 
+    s = 'http://www.onlinetvrecorder.com/v2/?go=list&tab=search'
+    s += '&order=beginn%20DESC' # neueste zuerst
+    s += '&preset='
+    s += '&epg_id='
+    s += '&start=' + str(x)
+    s += '&view='
+    s += '&state='
+    s += '&title=' + keyword
+    s += '&date=all'
+    s += '&cbsing=1'
+    if(station <> ''):
+        s += '&selectedstation_' + station + '=on'
+    s += '&source=otr'
+    s += '&intimefrom=0'
+    s += '&intimeto=23'
+    if(date == ''):
+        s += '&fd=1'
+        s += '&fm=1'
+        s += '&td=31'
+        s += '&tm=12'
+    else:
+        dd =date.split('.')
+        if(len(dd)==2):
+            day= "%02d" % int(dd[0])
+            month = "%02d" % int(dd[1])
+            s += '&fd=' + day
+            s += '&fm=' + month
+            s += '&td=' + day
+            s += '&tm=' + month
+        else:
+            s += '&fd=1'
+            s += '&fm=1'
+            s += '&td=31'
+            s += '&tm=12'
+    s += '&genre='
+    s += '&weekday='
+    s += '&selected_filter_weekday='
+    s += '&times='
+    s += '&selected_filter_times='
+    s += '&year=2016'
+    s += '&productionyear='
+    s += '&selected_filter_productionyear='
+    s += '&format=mp4,mcut,avi,hq,hcut,' # stream format
+    s += '&selected_filter_format='
+    s += '&filestate='
+    s += '&selected_filter_filestate='
+    s += '&minutes='
+    s += '&selected_filter_duration='
+    s += '&wdh='
+    s += '&season='
+    s += '&selected_filter_season='
+    s += '&episode='
+    s += '&selected_filter_episode='
+    s += '&rating='
+    s += '&selected_filter_rating='
+    s += '&fsk='
+    s += '&selected_filter_fsk='
+    s += '&actor='
+    s += '&director='
+    s += '&btn_ok=+Suchen+'
+    s += '&programm=0'
+    s += '&multioption='
+    
+    return s
---
+++ resources/lib/highlights.py
@@ -0,0 +1,414 @@
+import mechanize, re, urllib, time
+from BeautifulSoup import BeautifulSoup
+import functions
+
+class ItemClass(object):
+    pass
+
+def login(user, pw):
+    
+    x = ItemClass()
+    
+    # init browser
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+
+    br.open("http://www.onlinetvrecorder.com/v2/?go=home")
+
+    # login
+    br.select_form('fhomelogin')
+
+    br['email'] = user
+    br['password'] = pw
+    br.submit().read()
+
+    response = br.open("    ")
+    result = response.read()
+    
+    # now check state
+    x.state = 'not loged in'
+    x.id = ''
+      
+    # info on java scipt
+    match = re.search('my_user_id="(?P<id>.*?)";.*?my_ut="(?P<state>.*?)"', result)
+    if(match != None):
+        x.id = match.group('id')
+        x.state = match.group('state')
+           
+    x.decode = 'n.v.' 
+        
+    # info on html
+    match = re.search('<a.href=".?go=history&tab=decodings(.*?)<\/i>(?P<value>[^<]*)<', result)
+    if(match != None):
+        x.decode = match.group('value')
+            
+    x.value = 'n.a.'
+            
+    match = re.search('<div.id="cssmenuright">.*?<a.href=".go=points.*?>(?P<value>[^<]*)<', result, re.DOTALL)
+    if(match != None):
+        x.value = match.group('value')
+            
+    return x
+
+def getData(user, pw):
+
+    itemlist = []
+    
+    # get HTML
+    link = 'http://www.onlinetvrecorder.com/v2/?go=home'
+    data = functions.getHTML(user, pw, link)    
+ 
+    # logged in
+    result = data.replace('\'','\"')
+    soup = BeautifulSoup(result)
+
+    # search for highlights
+    
+    tables = soup.findAll('div' , {'class' : 'content'} ) 
+    
+    for table in tables:
+        # check its the right table
+        taList = table.find('div' , {'class' : 'homedoublehighlight'})
+        if taList is not None:
+
+            x = ItemClass()
+        
+            taList = table.find('td')
+            sStyle = taList['style'].encode()
+             
+            m = re.search('background-image:url\((?P<thumb>.*?)\)', sStyle)
+            if(m is not None):
+                x.thumb = m.group('thumb')
+            else:
+                x.thumb = 'DefaultVideo.png' 
+        
+            h1 = table.find('a')            
+            x.url = h1['href']
+            
+            # we just want the id   
+            s = x.url.index('id=')
+            x.url = x.url[s+3:]
+            
+            sp = table.find('span')
+            x.title = sp.text
+
+            text1 = table.find('div' , {'class' : 'homedoublehighlight'} )
+            x.text = text1.text
+            
+            x.vid = ''
+            
+            itemlist.append(x)
+
+    # search for actual movies
+    
+    content = soup.findAll ('div' , {'class' : 'homethree'} )
+
+    for c in content:
+        
+        x = ItemClass()
+    
+        link = c.find('a', {'class' : 'homethreehredbig'} )
+        if link is None:
+            break
+        
+        x.url = link['href']
+        
+        # we just want the id   
+        s = x.url.index('id=')
+        x.url = x.url[s+3:]
+
+        title = c.find('div' , {'class' : 'toolbardiv'} )
+        x.title = title.text
+    
+        data = c.findAll('div', {'class' : 'homethreee'} )
+
+        x.thumb = 'DefaultVideo.png' 
+        x.vid = ''
+
+        for e in data:
+            img = e.find('img')
+            if img is not None:
+                x.thumb = img['src']
+            else:
+                sty = e['style']
+                m = re.search('background-image:url\((?P<thumb>.*?)\)', sty)
+                if(m is not None):
+                    x.thumb = m.group('thumb')
+
+            vid = e.find('video')
+            if vid is not None:
+                x.vid = vid['src']
+    
+        desc = c.find('div' , {'class' : 'homethreec'} )
+        x.text = desc.text
+        
+        itemlist.append(x)    
+    
+    return itemlist
+    
+def getMoreData(user, pw, page):
+
+    # page 1 display page without big highlights
+    
+    itemlist = []
+
+    # init browser
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+
+    br.open("http://www.onlinetvrecorder.com/v2/?go=home")
+
+    # login
+    br.select_form('fhomelogin')
+
+    br['email'] = user
+    br['password'] = pw
+    br.submit().read()
+    
+    select = 0 # -2 returns also the highlights
+    
+    if(page == 2):
+        select = 14
+    if(page > 2):
+        select = (15 * (page-1)) - 1
+        
+    params = {u'language': 'de', u'start': str(select)}
+    data = urllib.urlencode(params)
+
+    response = br.open("http://www.onlinetvrecorder.com/v2/ajax/get_homethree.php",  data)
+    result = response.read() 
+
+    # logged in
+
+    result = result.replace('\'','\"')
+    soup = BeautifulSoup(result)
+
+    # search for actual movies
+
+    content = soup.findAll ('div' , {'class' : 'homethree'} )
+
+    for c in content:
+        
+        x = ItemClass()
+    
+        link = c.find('a', {'class' : 'homethreehredbig'} )
+        if link is None:
+            break
+        
+        x.url = link['href']
+        
+        # we just want the id   
+        s = x.url.index('id=')
+        x.url = x.url[s+3:]
+
+        title = c.find('div' , {'class' : 'toolbardiv'} )
+        x.title = title.text
+    
+        data = c.findAll('div', {'class' : 'homethreee'} )
+
+        x.thumb = 'DefaultVideo.png' 
+
+        for e in data:
+            img = e.find('img')
+            if img is not None:
+                x.thumb = img['src']
+            else:
+                sty = e['style']
+                m = re.search('background-image:url\((?P<thumb>.*?)\)', sty)
+                if(m is not None):
+                    x.thumb = m.group('thumb')
+               
+            vid = e.find('video')
+            if vid is not None:
+                x.vid = vid['src']
+    
+        desc = c.find('div' , {'class' : 'homethreec'} )
+        x.text = desc.text
+        
+        itemlist.append(x)    
+    
+    return itemlist
+    
+def getMovies(user, pw, epg_id):
+
+    itemlist = []
+
+    # get HTML
+    link = 'http://www.onlinetvrecorder.com/v2/?go=download&epg_id=' + epg_id 
+    data = functions.getHTML(user, pw, link)    
+    
+    result = data.replace('\'','\"')
+    
+    #text_file = open("c:\\temp\\005_2.htm", "w")
+    #text_file.write(data)
+    #text_file.close()
+    
+    # search the title & text
+    
+    title = '?'
+    match = re.search('<div.style="color:#FFF;.*?font-size:20px;([^>]*)>(?P<txt>[^>]*)<', result, re.DOTALL)
+    if(match is not None):
+        title = match.group('txt').strip()
+
+    desc = '...'
+    match = re.search('<div.style="height:215px;.overflow:auto([^>]*)>(?P<txt>[^>]*)<', result, re.DOTALL)
+    if(match is not None):
+        desc = match.group('txt').strip()
+    
+
+    # search picture   
+    thumb = 'DefaultVideo.png' 
+
+    match = re.search('<div.id="quickbigimage.*?src="(?P<thumb>[^"]*)"', result)
+    if(match != None):
+        thumb = match.group('thumb')
+    
+    # search preview video
+    
+    match = re.search('<video.id="previewvideo[^>]*>.*?<source.src="(?P<url>[^"]*)"', result, re.DOTALL)
+    if(match != None):
+        x = ItemClass()
+        x.url = match.group('url')
+        x.title = "Preview"
+        x.price = "0,00 Cent"
+        x.thumb = thumb
+        x.desc = desc
+        
+        itemlist.append(x)
+    
+    # search video page  
+                   
+    regex = '<input.type=button.class=epgscreengreenbutton[^>]*startEpgScreenStream\((?P<url>[^)]*)\);".value="(?P<q>.*?)">.*?<td.style=[^>]*>(?P<price>[^<]*)<'
+    
+    for m in re.finditer(regex, result, re.DOTALL): 
+        
+        test = m.group('q')
+        test = test.replace('&raquo;','')
+                        
+        x = ItemClass()
+            
+        x.url = m.group('url')
+        x.title = test
+        x.price = m.group('price')
+        x.thumb = thumb
+        x.desc = desc
+            
+        itemlist.append(x)  
+            
+    return itemlist    
+        
+def search(user, pw, keyword, page):
+           
+    link = functions.getSearchString(keyword, page)
+    data = functions.getHTML(user, pw, link)
+    
+    # now parse
+    return functions.scanList(data)
+
+def searchStation(user, pw, keyword, station, date, page):
+           
+    link = functions.getSearchStationString(keyword, station, date, page)
+    data = functions.getHTML(user, pw, link)
+          
+    # now parse
+    return functions.scanList(data)
+
+def getPlayLink(user, pw, eid, rid, mode):
+    
+    # init browser
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+
+    br.open("http://www.onlinetvrecorder.com/v2/?go=home")
+
+    # login
+    br.select_form('fhomelogin')
+
+    br['email'] = user
+    br['password'] = pw
+    br.submit().read()
+    
+    # logged in
+        
+    params = {u'eid': eid, u'rid': rid, u'mode': mode}
+    data = urllib.urlencode(params)
+
+    response = br.open("http://www.onlinetvrecorder.com/v2/ajax/start_epg_screen_stream.php",  data)
+    result = response.read() 
+
+    q = result[:2]
+    if(q == 'OK'):
+        
+        # result is OK
+        url = result[3:]
+        
+        response = br.open(url)
+        result = response.read()
+        
+        if mode <> 'normal':
+                
+            wait = 10; # default, we need to wait
+        
+            m = re.search('waittime=(?P<time>[^;]*);', result)
+            if m is not None:
+                t = m.group('time')
+                wait = int(t)
+        
+            wait = wait +1;
+        
+            while wait > 0:
+                time.sleep(1)
+                wait = wait - 1
+        
+            # now reload as the script
+            response = br.reload()
+            result = response.read() 
+        
+        m = re.search('source.src="(?P<url>[^"]*)"', result)
+        if m is not None:
+            t = m.group('url')
+            return t
+        
+        m = re.search('init_player."(?P<url>[^"]*)"', result)
+        if m is not None:
+            t = m.group('url')
+            return t
+        
+        return None
+
+def searchGroup(user, pw, group, page):
+    
+    link = functions.getGroupString(group, page)
+    data = functions.getHTML(user, pw, link)
+    
+    # now parse
+    return functions.scanList(data)
+
+def getRecords(user, pw, page):
+    
+    # get HTML
+    link = functions.getRecordsString(page)
+    data = functions.getHTML(user, pw, link)
+    
+    #text_file = open("c:\\temp\\005.htm", "w")
+    #text_file.write(data)
+    #text_file.close()
+    
+    # now parse
+    return functions.scanList(data)
+
+def getList(user, pw, no, page):
+    
+    # calculate start
+    iPage = int(page)
+    x = (iPage - 1) * 20
+    
+    # get HTML
+    link = "http://www.onlinetvrecorder.com/v2/?go=list&tab=toplist&listid=" +  no + "&start=" + str(x)
+    data = functions.getHTML(user, pw, link)
+    
+    # now parse
+    return functions.scanList(data)
+
+
+
